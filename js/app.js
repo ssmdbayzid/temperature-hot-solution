0f66b06f7b3a72a57cf14d14bf50131b0b3ca805
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-03-04 10:56:09 +0600

    add clicked

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/* API Link:  api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} */
+
+const API_KEY = `8199be7fd98dce7bba176bc002888350`;
+const loadData = () => {
+    console.log('i am click');
+}
