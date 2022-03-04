e9d2333379a78c4d270374c3588509601510e6fa
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-03-04 19:10:19 +0600

    Set City name & Temp

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 /* API Link:  api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} */
 
-const API_KEY = `8199be7fd98dce7bba176bc002888350`;
+
 const loadData = () => {
-    console.log('i am click');
+    const API_KEY = `8199be7fd98dce7bba176bc002888350`;
+    const city = document.getElementById('city').value;
+
+    const url   = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+//  console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(city => displayTempareture(city))
+}
+
+const cityInnerText = (id, text, isAdd) => {
+    if(isAdd == true){
+      document.getElementById(id).innerText = text;  
+    }
+    else{
+        document.getElementById(id).innerText = text;
+    }
+    
+
+}
+
+const displayTempareture = tempareture => {
+    console.log(tempareture);
+    cityInnerText('cityName', tempareture.name, true);
+    cityInnerText('temp', tempareture.main.temp, false);
+    
+    /* const city = document.getElementById('cityName'); this is type 1
+    city.innerText = tempareture.name;
+    */
 }
